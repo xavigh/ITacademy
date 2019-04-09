229e188d84fd655f9cf62028b3398cd16972c5f4
xaviGH <fgargallo@gmail.com>
2019-04-09 15:14:12 +0200

    commit Basic js exercise, text document attached.
    Signed-off-by: xaviGH <fgargallo@gmail.com>

--- ex5_js/js/basic.js
+++ ex5_js/js/basic.js
@@ -25,6 +25,54 @@ return total= x + y;
 }
 
 //ex 5
-function nota_exam(){
-  
+function nota_exam(a){
+  if(a >= 5){
+    return  document.getElementById('demo').innerHTML = "you have passed the exam with a "+ a;
+  }
+  else{
+    return document.getElementById('demo').innerHTML = "you have failed the exam "+ a;
+  }
+}
+
+//ex6 replace strings
+
+function replaceFun(a,b){
+
+  var str= document.getElementById("demo").innerHTML;
+  var txt = str.replace(a, b);
+  return document.getElementById('demo').innerHTML = str +"<br>"+ txt;
+
+}
+
+function objects(){
+
+  var coses = ['Taula', 'cadira','ordinador', 'llibreta'];
+
+  for (var i=0; i < coses.length; i++){
+    txt = document.getElementById("demo").innerHTML;
+    document.getElementById("demo").innerHTML = txt + coses[i] + "  position is "+ i +"<br>";
+  }
+
+return txt;
+
+}
+// function calculator
+function calculator(opt, val1, val2){
+
+      switch(opt){
+          case "sum":
+             txt = document.getElementById('demo').innerHTML;
+
+             document.getElementById('demo').innerHTML = txt +" "+ (val1 + val2);
+                break;
+          case "rest":
+               txt = document.getElementById('demo').innerHTML;
+              document.getElementById('demo').innerHTML =txt +" "+ (val1 - val2);
+                break;
+          case "mult":
+               txt = document.getElementById('demo').innerHTML;
+              document.getElementById('demo').innerHTML = txt +" "+ (val1 * val2);
+                break;
+    }
+
 }
