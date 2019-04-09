e49fc1560a812f1ec3043ff763c0057c49775a7b
xaviGH <fgargallo@gmail.com>
2019-04-09 11:36:51 +0200

    EX5, Basic Javascript

---
+++ ex5_js/js/basic.js
@@ -0,0 +1,30 @@
+
+
+//ex1
+//console.log("hello world");
+
+//ex2
+// alert("my name is Xavi !");
+
+//ex3
+// var name, surname;
+// name = "Xavi";
+// surname = "Gargallo Jané";
+//document.write(name + " " + surname);
+
+//ex4 sum of 2 numbers
+
+
+
+function sum(){
+  var x = 5;
+  var y = 5;
+  var total = x + y;
+return total= x + y;
+
+}
+
+//ex 5
+function nota_exam(){
+  
+}
